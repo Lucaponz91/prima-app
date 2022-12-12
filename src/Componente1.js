f4bbcfce8d7b2f471079ab988d4f4397a779d409
Lucaponz91 <lucapo1991@msn.com>
2022-12-13 00:19:50 +0100

    inizio

---
+++ src/Componente1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Componente1 = () => {
+    return (
+        <React.Fragment>
+            <div>Componente1</div>
+            <div>Ciaociao sempre in componente 1</div>
+            
+        </React.Fragment>
+        );
+};
+
+export default Componente1
